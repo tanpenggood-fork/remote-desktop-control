392fcd8ac584f8c6f7b15eac17d72a46ff1f6fd9
unknown <2267431887@qq.com>
2024-12-20 09:30:43 +0800

    bug fix

--- client/src/main/java/io/github/springstudent/dekstop/client/clipboard/ClipboardPoller.java
+++ client/src/main/java/io/github/springstudent/dekstop/client/clipboard/ClipboardPoller.java
@@ -25,8 +25,6 @@ public class ClipboardPoller {
 
     private Thread pollerThread;
 
-    private volatile boolean toStop = false;
-
     public void addListener(ClipboardListener listener) {
         listeners.add(listener);
     }
@@ -34,7 +32,7 @@ public class ClipboardPoller {
     public void start() {
         Log.info("client start clipboard poller");
         pollerThread = new Thread(() -> {
-            while (!toStop) {
+            while (true) {
                 try {
                     checkClipboard();
                 } catch (Exception e) {
@@ -43,9 +41,7 @@ public class ClipboardPoller {
                 try {
                     TimeUnit.MILLISECONDS.sleep(1000);
                 } catch (InterruptedException e) {
-                    if (!toStop) {
-                        Log.error(e.getMessage(), e);
-                    }
+                    pollerThread.interrupt();
                 }
             }
         });
@@ -56,52 +52,42 @@ public class ClipboardPoller {
 
     public void stop() {
         Log.info("client stop clipboard poller");
-        toStop = true;
         if (pollerThread == null) {
             return;
         }
         pollerThread.interrupt();
-        try {
-            pollerThread.join();
-        } catch (InterruptedException e) {
-            Log.error(e.getMessage(), e);
-        }
     }
 
 
     private void checkClipboard() throws Exception {
-        try {
-            Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
-            if (clipboard.isDataFlavorAvailable(DataFlavor.stringFlavor)) {
-                String currentText = (String) clipboard.getData(DataFlavor.stringFlavor);
-                if (!currentText.equals(lastText)) {
-                    Log.info("Clipboard contains new text: " + currentText);
-                    lastText = currentText;
-                    fireClipboardText(currentText);
-                }
+        Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+        if (clipboard.isDataFlavorAvailable(DataFlavor.stringFlavor)) {
+            String currentText = (String) clipboard.getData(DataFlavor.stringFlavor);
+            if (!currentText.equals(lastText)) {
+                Log.info("Clipboard contains new text: " + currentText);
+                lastText = currentText;
+                fireClipboardText(currentText);
             }
-            if (clipboard.isDataFlavorAvailable(DataFlavor.imageFlavor)) {
-                BufferedImage currentImage = (BufferedImage) clipboard.getData(DataFlavor.imageFlavor);
-                if (currentImage != lastImage) {
-                    Log.info("Clipboard contains new image: " + currentImage.getWidth() + "x" + currentImage.getHeight());
-                    lastImage = currentImage;
-                    fireClipboardImg(currentImage);
-                }
-            }
-            if (clipboard.isDataFlavorAvailable(DataFlavor.javaFileListFlavor)) {
-                List<File> currentFiles = (List<File>) clipboard.getData(DataFlavor.javaFileListFlavor);
-                if (currentFiles != null && !currentFiles.equals(lastFiles)) {
-                    Log.info("Clipboard contains new files:");
-                    for (File file : currentFiles) {
-                        System.out.println(file.getAbsolutePath());
-                    }
-                    lastFiles = currentFiles;
-                }
-            }
-
-        } catch (Exception e) {
-            throw e;
         }
+        if (clipboard.isDataFlavorAvailable(DataFlavor.imageFlavor)) {
+            BufferedImage currentImage = (BufferedImage) clipboard.getData(DataFlavor.imageFlavor);
+            if (currentImage != lastImage) {
+                Log.info("Clipboard contains new image: " + currentImage.getWidth() + "x" + currentImage.getHeight());
+                lastImage = currentImage;
+                fireClipboardImg(currentImage);
+            }
+        }
+        if (clipboard.isDataFlavorAvailable(DataFlavor.javaFileListFlavor)) {
+            List<File> currentFiles = (List<File>) clipboard.getData(DataFlavor.javaFileListFlavor);
+            if (currentFiles != null && !currentFiles.equals(lastFiles)) {
+                Log.info("Clipboard contains new files:");
+                for (File file : currentFiles) {
+                    System.out.println(file.getAbsolutePath());
+                }
+                lastFiles = currentFiles;
+            }
+        }
+
     }
 
     private void fireClipboardText(String text) {
