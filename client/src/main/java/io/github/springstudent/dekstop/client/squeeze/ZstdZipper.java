de7cfefbb6be6145304bf0b1eb91094dad81e4d6
unknown <2267431887@qq.com>
2025-02-19 13:22:04 +0800

    optimize code

--- client/src/main/java/io/github/springstudent/dekstop/client/squeeze/ZstdZipper.java
+++ client/src/main/java/io/github/springstudent/dekstop/client/squeeze/ZstdZipper.java
@@ -14,7 +14,6 @@ import java.io.IOException;
 public class ZstdZipper implements Zipper {
     @Override
     public MemByteBuffer zip(MemByteBuffer unzipped) throws IOException {
-        // 使用 ByteArrayOutputStream 来捕获压缩后的数据
         try (MemByteBuffer zipped = new MemByteBuffer()) {
             try (ZstdOutputStream zstdOutputStream = new ZstdOutputStream(zipped)) {
                 zstdOutputStream.write(unzipped.getInternal(), 0, unzipped.size());
@@ -28,8 +27,7 @@ public class ZstdZipper implements Zipper {
     public MemByteBuffer unzip(MemByteBuffer zipped) throws IOException {
         try (final MemByteBuffer unzipped = new MemByteBuffer()) {
             try (ZstdInputStream zstdInputStream = new ZstdInputStream(new ByteArrayInputStream(zipped.getInternal(), 0, zipped.size()))) {
-                // 将 ZstdInputStream 的数据读取并写入到输出流中，Zstd 会进行解压
-                byte[] buffer = new byte[4096];  // 使用缓冲区，避免频繁读写
+                byte[] buffer = new byte[4096];
                 int bytesRead;
                 while ((bytesRead = zstdInputStream.read(buffer)) > 0) {
                     unzipped.write(buffer, 0, bytesRead);
