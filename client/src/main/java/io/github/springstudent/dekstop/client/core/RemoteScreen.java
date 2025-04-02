2c6acdbd93c7445537944e48910d7fd2f369869d
unknown <2267431887@qq.com>
2025-04-02 13:29:26 +0800

    fix

--- client/src/main/java/io/github/springstudent/dekstop/client/core/RemoteScreen.java
+++ client/src/main/java/io/github/springstudent/dekstop/client/core/RemoteScreen.java
@@ -174,6 +174,11 @@ public class RemoteScreen extends JFrame {
     private JMenu createScreenSelectionMenu(int screenNum) {
         this.selectScreenMenu = new JMenu("屏幕切换");
         ButtonGroup screenGroup = new ButtonGroup();
+        if (screenNum > 1) {
+            JRadioButtonMenuItem screenItem = new JRadioButtonMenuItem("所有屏幕");
+            screenItem.addActionListener(e -> RemoteClient.getRemoteClient().getController().sendScreenSelect(-1));
+            selectScreenMenu.add(screenItem);
+        }
         for (int i = 0; i < screenNum; i++) {
             JRadioButtonMenuItem screenItem = new JRadioButtonMenuItem("屏幕 " + (i + 1));
             final int screenIndex = i;
@@ -397,7 +402,7 @@ public class RemoteScreen extends JFrame {
         if (selectScreenMenu != null) {
             optionsMenu.remove(selectScreenMenu);
         }
-        if (screenNum > 1) {
+        if (screenNum > 0) {
             this.optionsMenu.add(createScreenSelectionMenu(screenNum));
         }
         long sessionStartTime = Instant.now().getEpochSecond();
