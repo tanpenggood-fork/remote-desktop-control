a72106d86669da6d6f394da9cbf34fa896e2b31b
unknown <2267431887@qq.com>
2025-04-03 08:24:01 +0800

    ff

--- client/src/main/java/io/github/springstudent/dekstop/client/RemoteClient.java
+++ client/src/main/java/io/github/springstudent/dekstop/client/RemoteClient.java
@@ -166,7 +166,7 @@ public class RemoteClient extends RemoteFrame {
 
 
     public static void main(String[] args) throws Exception {
-        RemoteClient remoteClient = new RemoteClient("192.168.0.109", 54321, "http://192.168.0.109:12345/remote-desktop-control");
+        RemoteClient remoteClient = new RemoteClient("172.16.1.37", 54321, "http://172.16.1.37:12345/remote-desktop-control");
     }
 
 }
