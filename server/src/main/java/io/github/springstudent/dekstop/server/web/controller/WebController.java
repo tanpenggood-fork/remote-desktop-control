b20240c95a9116d7462117e2daccf9c3e24c0832
tanpenggood <tanpengswpu@163.com>
2025-05-25 21:44:53 +0800

    feat(server): Add API /web/unbindChannelBrother/{controlledDeviceCode}

--- server/src/main/java/io/github/springstudent/dekstop/server/web/controller/WebController.java
+++ server/src/main/java/io/github/springstudent/dekstop/server/web/controller/WebController.java
@@ -1,13 +1,18 @@
 package io.github.springstudent.dekstop.server.web.controller;
 
+import io.github.springstudent.dekstop.common.command.CmdReqCapture;
+import io.github.springstudent.dekstop.server.netty.NettyChannelBrother;
 import io.github.springstudent.dekstop.server.netty.NettyChannelManager;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 
 /**
  * @author tanpenggood
@@ -23,6 +28,14 @@ public class WebController {
         result.put("deviceCodeChannel", NettyChannelManager.getDeviceCodeChannelMap());
         result.put("channelBrother", NettyChannelManager.getChannelBrotherMap());
         return ResponseEntity.ok(result);
+    }
+
+    @GetMapping("/unbindChannelBrother/{controlledDeviceCode}")
+    public ResponseEntity<Map<String, NettyChannelBrother>> unbindChannelBrother(@PathVariable String controlledDeviceCode) {
+        Map<String, NettyChannelBrother> channelBrotherMap = NettyChannelManager.getChannelBrotherMap();
+        NettyChannelBrother channelBrother = channelBrotherMap.get(controlledDeviceCode);
+        Optional.ofNullable(channelBrother).ifPresent(s -> NettyChannelManager.unbindChannelBrother(CmdReqCapture.STOP_CAPTURE, channelBrother.getControlled()));
+        return ResponseEntity.ok(Collections.singletonMap(controlledDeviceCode, channelBrother));
     }
 
 }
